c22ef04e71e615553f2f03471906319e63785d01
Prashant Kumar <144902971+Prashant2raja@users.noreply.github.com>
2025-05-17 11:45:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -525,5 +525,5 @@ app.get('/test-db', async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
